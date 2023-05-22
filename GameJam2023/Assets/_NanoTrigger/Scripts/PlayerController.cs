18ba13f7f498f9f78bbc1b283d4ba6893fa5c3c3
mithrauko <34655326+mithrauko@users.noreply.github.com>
2023-05-21 19:48:29 -0600

    testing inputcontroller

--- GameJam2023/Assets/_NanoTrigger/Scripts/PlayerController.cs
+++ GameJam2023/Assets/_NanoTrigger/Scripts/PlayerController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D rigidbody;
+
+    public float MoveSpeed;
     // Start is called before the first frame update
     void Start()
     {
         
+        rigidbody = this.GetComponent<Rigidbody2D>();        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnMove(InputValue value)
+    {
+        // Read value from control. The type depends on what type of controls.
+        // the action is bound to.
+        Debug.Log("move");
+        Vector2 v = value.Get<Vector2>();
+        rigidbody.AddForce(v*MoveSpeed, ForceMode2D.Force);        
+        // IMPORTANT:
+        // The given InputValue is only valid for the duration of the callback. Storing the InputValue references somewhere and calling Get<T>() later does not work correctly.
     }
 }
