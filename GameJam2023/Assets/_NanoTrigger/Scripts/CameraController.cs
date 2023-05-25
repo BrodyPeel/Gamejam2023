24f71ce77581952d33506f9ea5b9ef610ba67037
Brody Peel <brodypeel@gmail.com>
2023-05-25 15:20:45 -0600

    Updated COntrols

--- GameJam2023/Assets/_NanoTrigger/Scripts/CameraController.cs
+++ GameJam2023/Assets/_NanoTrigger/Scripts/CameraController.cs
@@ -6,17 +6,10 @@ public class CameraController : MonoBehaviour
     public Transform ship;
     public float smoothSpeed = 0.125f;
 
-    private Vector3 offset;
-
-    private void Start()
-    {
-        offset = transform.position - ship.position;
-    }
-
     private void LateUpdate()
     {
         Vector3 targetPosition = new Vector3(ship.position.x, ship.position.y, transform.position.z);
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition + offset, smoothSpeed * Time.deltaTime);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
 
